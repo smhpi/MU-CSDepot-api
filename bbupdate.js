d09d48379db712153cb81df2a50969cef31bdab2
Hosen Poursaeid <poursaeid@outlook.com>
2019-01-31 07:23:25 -0500

    last-updated

---
+++ bbupdate.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "notes",
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    },
+    UpdateExpression: `SET bbq=:bbq, price=:price`,
+    ExpressionAttributeValues: {
+      ":bbq": data.quantity || null,
+      ":price": data.price || null
+    },
+    ReturnValues: "ALL_NEW"
+  };
+
+  try {
+    const result = await dynamoDbLib.call("update", params);
+    return success({ status: true });
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
